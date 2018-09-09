52b13c68dae2dfb6849107a50ec5f79a063b31d9
David Aghassi <david.aghassi@gmail.com>
2018-09-09 10:34:15 -0700

    build: added husky and lint-staged

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.export = {
-    "eslint-config-tools": "lib/eslint-config-tools.js",
-    "eslint-config-apps": "lib/eslint-config-apps.js",
-    "prettier": "lib/prettier-config.js"
-}
+  'eslint-config-tools': './lib/eslint-config-tools.js',
+  'eslint-config-apps': './lib/eslint-config-apps.js',
+  prettier: './lib/prettier-config.js',
+};
