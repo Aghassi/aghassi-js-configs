1db0d072df59b92da53062bce3c87c75fb947609
David Aghassi <3680126+Aghassi@users.noreply.github.com>
2018-10-07 10:20:54 -0700

    feat: added configs to index.js exports

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.export = {
-  'eslint-config-tools': './lib/eslint-config-tools.js',
   'eslint-config-apps': './lib/eslint-config-apps.js',
-  prettier: './lib/prettier-config.js',
+  'eslint-config-tools': './lib/eslint-config-tools.js',
+  'husky-config': './lib/husky-config',
+  'lint-staged-config': './lib/lint-staged.config',
+  'prettier-config': './lib/prettier-config.js',
+  'semantic-release-config': './lib/semantic-release-config',
 };
