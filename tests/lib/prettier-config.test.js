6ed4b5b16e6b4fe42764825143173d690011ac95
David Aghassi <3680126+Aghassi@users.noreply.github.com>
2018-10-08 22:44:09 -0700

    test: added prettier config

---
+++ tests/lib/prettier-config.test.js
@@ -0,0 +1,5 @@
+const prettierConfig = require('../../lib/prettier-config');
+const validateConfig = require('./__helpers__/configSnapshot');
+
+// Use the helper suite to generate a test suite to run
+validateConfig('prettier', prettierConfig);
