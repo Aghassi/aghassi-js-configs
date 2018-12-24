1885f4c91bf7fdfe50daa61ff5997f76efef4415
David Aghassi <3680126+Aghassi@users.noreply.github.com>
2018-12-24 08:52:15 -0800

    fix: required module paths instead of just providing them

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const commitlintConfig = require('./lib/commitlint.config');
+const eslintConfigApp = require('./lib/eslint-config-apps');
+const eslintConfigTools = require('./lib/eslint-config-tools');
+const huskyConfig = require('./lib/husky.config');
+const lintStagedConfig = require('./lib/lint-staged.config');
+const prettierConfig = require('./lib/prettier.config');
+const semanticReleaseConfig = require('./lib/semantic-release.config');
+
 module.export = {
-  commitlint: './lib/commitlint.config',
-  'eslint-config-apps': './lib/eslint-config-apps',
-  'eslint-config-tools': './lib/eslint-config-tools',
-  husky: './lib/husky.config',
-  'lint-staged': './lib/lint-staged.config',
-  prettier: './lib/prettier.config',
-  'semantic-release': './lib/semantic-release.config',
+  commitlint: commitlintConfig,
+  'eslint-config-apps': eslintConfigApp,
+  'eslint-config-tools': eslintConfigTools,
+  husky: huskyConfig,
+  'lint-staged': lintStagedConfig,
+  prettier: prettierConfig,
+  'semantic-release': semanticReleaseConfig,
 };
